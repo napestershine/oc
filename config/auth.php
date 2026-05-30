434b75c1dadf2491006c83681fc2b1871d19dc79
Manu <napestershine@gmail.com>
2026-05-30 09:46:51 +0200

    fix tests: add unverified state to factory, fix password reset config
    - Add email_verified_at and unverified() state to UserFactory
    - Rename password_resets table to password_reset_tokens
    - Fix auth config password broker table reference

--- config/auth.php
+++ config/auth.php
@@ -98,9 +98,9 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'email' => 'auth.emails.password',
-            'table' => 'password_resets',
+            'table' => 'password_reset_tokens',
             'expire' => 60,
+            'throttle' => 60,
         ],
     ],
 
